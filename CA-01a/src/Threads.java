da91be14077e4b5a117e60590d559ccae61be3c3
Rasmus <Rasmus@10.50.130.148>
2015-08-25 10:59:32 +0200

    Start

---
+++ CA-01a/src/Threads.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rasmus
+ */
+public class Threads {
+    public static void main(String[] args) {
+    
+        
+        
+        
+        
+    }    
+    
+    
+//private class Tasks implements Runnable {
+//
+//        @Override
+//        public void run() {
+//            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        }
+//    
+//    
+//    
+//}
+    }
+    
+    
+   
+
